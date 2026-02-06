7b45a4b081cd84a6076c3da9cc028a34e727579d
Darius <kdarius641@gmail.com>
2026-02-06 18:04:03 +0100

    Basis: Auslesen von Datein

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MP3_Sortierung
+{
+    class Program
+    {
+        public static string[] GetFiles(string path)
+        {
+            return System.IO.Directory.GetFiles(path);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starte MP3 Suche...");
+
+            string MusiKPfad = @"C:\Users\Darius\Music\";
+
+            string[] GefundenerPfad = GetFiles(MusiKPfad);
+
+            Console.WriteLine($"{GefundenerPfad.Length} Lieder gefunden in {MusiKPfad}");
+            Console.WriteLine("-----------------------------");
+
+            foreach (string Datei in GefundenerPfad)
+            {
+                Console.WriteLine(Datei);
+            }
+        }
+    }
+}
